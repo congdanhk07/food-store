529fa9344b0fcd6ea6c5f910598db0cfeec2e9c6
Cong Danh <danh.tran@dision.tech>
2025-01-15 09:26:14 +0700

    feat: add slider into homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 
 import classes from './page.module.css'
+import SlideShowImage from './components/slide-show/slide-show'
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <SlideShowImage />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
